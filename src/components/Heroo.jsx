f4d8e406e3113aa37875726c585e3c3b6eba9919
pratiksingh27 <pratik27march@gmail.com>
2022-08-20 16:56:06 +0530

    font size fixed in navbar

--- src/components/Heroo.jsx
+++ src/components/Heroo.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import Typed from 'react-typed';
+import Typed from 'react-typed';
 
 
 export default function Heroo() {
@@ -9,10 +9,10 @@ export default function Heroo() {
       <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto flex text-center flex-col justify-center'>
         <p className='text-[#00df9a] font-bold p-2'>TRAVEL WITH FUN &#9992;</p>
         <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold md:py-2'>Safar Apka, Khayal humara</h1>
-        {/* <div className='flex justify-center items-center pt-1'>
+        <div className='flex justify-center items-center pt-1'>
           <p className='md:text-2xl sm:text-1.5xl text-xl font-bold'>We offer </p>
           <Typed className='md:text-2xl sm:text-1.5xl text-xl font-bold pl-2' strings={[' Best deal','Best services', 'Best package']} typeSpeed={40} backSpeed={40} loop/>
-        </div> */}
+        </div>
         <p className='md:text-xl sm:text-md text-gray-400 pt-2'>For exciting offer and discount, Register now</p>
         <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-4 mx-auto py-1 text-black'>Register Now</button>
       </div>
--- src/components/Menubar.jsx
+++ src/components/Menubar.jsx
@@ -11,7 +11,7 @@ export default function Menubar() {
   }
   return (
     <div className='flex  justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
-      <h1 className='w-full text-3xl font-bold text-[#00df9a]'>Shubh_Yatra</h1>
+      <h1 className='w-full text-2xl font-bold text-[#00df9a]'>Shubh_Yatra</h1>
       <ul className='hidden md:flex'>
         <li className='p-4'>Home</li>
         <li className='p-4'>About</li>
