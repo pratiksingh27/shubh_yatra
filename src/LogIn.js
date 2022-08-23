f8f539cf85b8b908ea610d4cc01497349bcb2923
pratiksingh27 <pratik27march@gmail.com>
2022-08-24 00:01:23 +0530

    updated a login and registartion page to store data of user

--- src/LogIn.js
+++ src/LogIn.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import form from './assets/form.png'
 
 export default function LogIn() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [allEntry, setAllEntry] = useState([])
+
+  const submitForm = (e) =>{
+    e.preventDefault();
+    if(email && password){
+    const newEntry = { email: email, password: password };
+
+    setAllEntry([...allEntry, newEntry]);
+    console.log(allEntry);
+    // To clera data once form is submited
+    setEmail("");
+    setPassword("");}
+    else{
+      alert("Fill the data first")
+    }
+  }
   return (
     <>
 
@@ -11,7 +30,8 @@ export default function LogIn() {
         </div>
         <div>
           
-          <form
+          <form action=''
+          onSubmit={submitForm}
             className="lg:grid-cols-3 py-16 shadow-xl w-full px-8 md:mx-auto md:w-full rounded-lg">
             <h1 className="font-bold text-3xl md:2xl">Sign In</h1>
             
@@ -21,6 +41,8 @@ export default function LogIn() {
                 className="p-2 flex w-50% rounded-md text-black mr-2 md:w-full w-full"
                 type="email"
                 name="email" required
+                value={email} onChange={(e) => setEmail(e.target.value)}
+                
                 placeholder="Enter Your Email"
               /><hr></hr>
             </div>
@@ -30,6 +52,7 @@ export default function LogIn() {
                 className="p-2 flex w- rounded-md text-black mr-2 md:w-full w-full"
                 type="password"
                 name="password" required
+                value={password} onChange={(e) => setPassword(e.target.value)}
                 placeholder="Your Password" 
               /><hr></hr>
             </div>
@@ -41,6 +64,18 @@ export default function LogIn() {
             />
           </form>
         </div>
+      </div>
+      <div>
+        {
+          allEntry.map((curElem) => {
+            return(
+                <div className='showDataStyle text-white grid grid-cols-2 justify-between text-center'>
+                  <p>{curElem.email}</p>
+                  <p>{curElem.password}</p>
+                </div>
+            )
+          })
+        }
       </div>
     </>
   )
--- src/Registration.js
+++ src/Registration.js
@@ -1,20 +1,33 @@
-import React from "react";
-// import { useForm } from 'react-hook-form';
+import React, { useRef } from "react";
+import { useForm } from 'react-hook-form';
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import form from './assets/form.png'
 
 export default function Registration() {
+
+  
+
+
   // const { register, handleSubmit, errors } =useForm();
-  // const [userInfo, setUserInfo]=useState();
-  // const onSubmit = (data) => {
-  //   setUserInfo(data);
-  //   console.log(data);
-  // };
+  const {
+    register,
+    formState: { errors },
+    handleSubmit, watch
+  } = useForm({
+    // mode: "onBlur",
+  });
+  const password = useRef({});
+  password.current = watch("password", "");
+  const [userInfo, setUserInfo]=useState();
+  const onSubmit = (data) => {
+    setUserInfo(data);
+    console.log(data);
+  };
 const [user, setUser] = useState({
   name:"", email:"", phone:"", password:"", cpassword:""
 });
-  let name, value
+  let name, value;
   const handelInputs = (e) => {
       console.log(e);
       name = e.target.name;
@@ -22,10 +35,18 @@ const [user, setUser] = useState({
 
       setUser({...user, [name]:value});
   }
+  
+//   const handleClick =() =>{
+    
+//     alert("Registration Completed")
+//     window.location.reload(true);
+  
+// }
 
   return (
     <>
-    {/* <pre className="text-white">{JSON.stringify(userInfo, undefined, 4)}</pre> */}
+    <pre className="text-white text-center">{JSON.stringify(userInfo, undefined, 2)}
+    </pre>
       <div className="bg-white max-w-[1240px] mx-auto my-8 grid md:grid-cols-2">
         <div className="my-auto justify-center flex">
           <img className="grid align-middle mx-auto justify-center" src={form} alt="" />
@@ -33,7 +54,7 @@ const [user, setUser] = useState({
         <div>
           
           <form
-          //  onSubmit={ handleSubmit(onSubmit)}
+           onSubmit={ handleSubmit(onSubmit)}
             className="lg:grid-cols-3 py-16 shadow-xl w-full px-8 md:mx-auto md:w-full rounded-lg">
             <h1 className="font-bold text-3xl md:2xl">Sign Up</h1>
             <div className="py-4">
@@ -42,21 +63,24 @@ const [user, setUser] = useState({
                 type="text"
                 name="name"
                 value={user.name}
+                {...register( 'name', { required: true })}
                 onChange={handelInputs}
-                // ref={register()}
-                placeholder="Enter your Name" required
-              /><hr></hr>
+                placeholder="Enter your Name"
+              />
+              <hr></hr>
+              {errors.name && <p className="text-red-400 mt-1">Username is required</p>}
             </div>
             <div className="py-4 form-group">
               <input
                 className="p-2 flex w-50% rounded-md text-black mr-2 md:w-full w-full"
-                type="email"
+                type="text"
                 name="email"
                 value={user.email}
+                {...register("email", { required: true, pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{3,4}$/i })}
                 onChange={handelInputs}
-                //  ref={register()}
                 placeholder="Enter Your Email" 
               /><hr></hr>
+              {errors.email && <p className="text-red-400 mt-1">Email is invalid</p>}
             </div>
             <div className="py-4 from-group">
               <input
@@ -64,10 +88,11 @@ const [user, setUser] = useState({
                 type="number"
                 name="phone"
                 value={user.phone}
+                {...register("phone", {required: true, pattern: /^(?:(?:\+|0{0,2})91(\s*[\-]\s*)?|[0]?)?[6789]\d{9}$/ })}
                 onChange={handelInputs} 
-                // ref={register()}
                 placeholder="Your Phone" 
               /><hr></hr>
+              {errors.phone && <p className="text-red-400 mt-1">Number is invalid</p>}
             </div>
             <div className="py-4 form-group">
               <input
@@ -75,10 +100,11 @@ const [user, setUser] = useState({
                 type="password"
                 name="password"
                 value={user.password}
+                {...register("password", { required: true, pattern: /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,99}$/ })}
                 onChange={handelInputs}
-                //  ref={register()}
                 placeholder="Your Password" 
               /><hr></hr>
+              {errors.password && <p className="text-red-400 mt-1">Password should contane min. 6 words, Capital Letter, Small Letter , Number and Symbol</p>}
             </div>
             <div className="py-4 form-group">
               <input
@@ -86,16 +112,18 @@ const [user, setUser] = useState({
                 type="password"
                 name="cpassword"
                 value={user.cpassword}
+                {...register("cpassword", { required: true, validate: value => value===password.current })}
                 onChange={handelInputs}
-                //  ref={register()}
                 placeholder="Confirm Your Password" 
               /><hr></hr>
+              {errors.cpassword && <p className="text-red-400 mt-1">Confirm your password</p>}
             </div>
 
             <input
               className="bg-[#00df9a] w-[200px] rounded-md font-medium my-4 mx-auto py-1 text-black hover:opacity-70 hover:scale-105 duration-300"
               type="submit"
               value="Registre Now"
+              // onClick={handleClick}
             />
             
           </form>
